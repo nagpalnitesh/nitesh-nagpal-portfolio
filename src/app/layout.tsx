04398b2e43708092c3f9c3ca1783f5cb8a64d9ea
Nitesh Nagpal <niteshnagpal@outlook.com>
2024-08-26 23:30:41 +0530

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +9,21 @@ export const metadata: Metadata = {
   title: "Nitesh Nagpal",
   description:
     "Showcasing my journey as a full-stack developer, this portfolio highlights my passion for crafting innovative digital solutions that inspire and engage.",
+  keywords:
+    "software engineer, full stack developer, web3, ai, machine learning, mern stack, mongodb, reactjs, nextjs, postgresql, javascript, typescript, express",
+  openGraph: {
+    type: "website",
+    url: "https://niteshnagpal.com",
+    title: "Nitesh Nagpal",
+    description:
+      "Showcasing my journey as a full-stack developer, this portfolio highlights my passion for crafting innovative digital solutions that inspire and engage.",
+    siteName: "Nitesh Nagpal",
+    // images: [
+    //   {
+    //     url: "https://example.com/og.png",
+    //   },
+    // ],
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +35,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon"
+          type="image"
+          sizes="any"
+        />
       </head>
       <body className={inter.className}>
         <ThemeProvider
